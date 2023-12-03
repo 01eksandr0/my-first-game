f203ce08089c797542545e1d92d489733900327d
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-12-03 17:59:33 +0200

    2level

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const boxs = document.querySelector(".my-section");
+const boxTwo = document.querySelector(".my-section-two");
+const levelOne = document.querySelector(".level-one");
+const menuLevelTwo = document.querySelector(".menu-level-two");
 let totalFunction = 0;
+let totalSum = 0;
 let first, second;
 boxs.addEventListener("click", (e) => {
   if (e.target.classList.contains("cart")) {
@@ -19,12 +23,67 @@ boxs.addEventListener("click", (e) => {
       totalFunction += 1;
     }
     if (
+      totalFunction === 2 &&
       first.lastElementChild.lastElementChild.src ===
-      second.lastElementChild.lastElementChild.src
+        second.lastElementChild.lastElementChild.src
     ) {
       totalFunction = 0;
       first = "";
       second = "";
+      totalSum += 1;
+    } else if (totalFunction === 2 && first !== second) {
+      const fnRemove = () => {
+        first.classList.remove("flip");
+        first.lastElementChild.style.display = "none";
+        second.classList.remove("flip");
+        second.lastElementChild.style.display = "none";
+        totalFunction = 0;
+        first = "";
+        second = "";
+      };
+      setTimeout(fnRemove, 800);
+    }
+  }
+  if (totalSum === 6) {
+    totalSum = 0;
+    const fnNextLevel = () => {
+      levelOne.classList.toggle("is-sicret");
+    };
+    setTimeout(fnNextLevel, 500);
+    const fnNextLevelMenu = () => {
+      menuLevelTwo.classList.toggle("is-sicret");
+    };
+    setTimeout(fnNextLevelMenu, 1000);
+  }
+  console.log(totalSum);
+});
+// =========
+boxTwo.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cart")) {
+    if (totalFunction === 0) {
+      e.target.classList.add("flip");
+      const fnAddActive = () =>
+        (e.target.lastElementChild.style.display = "block");
+      setTimeout(fnAddActive, 100);
+      first = e.target;
+      totalFunction += 1;
+    } else if (totalFunction === 1) {
+      e.target.classList.add("flip");
+      const fnAddActive = () =>
+        (e.target.lastElementChild.style.display = "block");
+      setTimeout(fnAddActive, 100);
+      second = e.target;
+      totalFunction += 1;
+    }
+    if (
+      totalFunction === 2 &&
+      first.lastElementChild.lastElementChild.src ===
+        second.lastElementChild.lastElementChild.src
+    ) {
+      totalFunction = 0;
+      first = "";
+      second = "";
+      totalSum += 1;
     } else if (totalFunction === 2 && first !== second) {
       const fnRemove = () => {
         first.classList.remove("flip");
--- js/menu-hello-active.js
+++ js/menu-hello-active.js
@@ -1,9 +1,16 @@
 const btnEl = document.querySelector(".btn-hello");
 const menuHelloEl = document.querySelector(".menu-hello");
 const levelOne = document.querySelector(".level-one");
+const levelTwo = document.querySelector(".level-two");
+const btnTwoLevel = document.querySelector(".btn-two-level");
+const menuLevelTwo = document.querySelector(".menu-level-two");
 // =========
 btnEl.addEventListener("click", () => {
   menuHelloEl.classList.toggle("is-sicret");
   const fnToggClass = () => levelOne.classList.toggle("is-sicret");
   setTimeout(fnToggClass, 275);
+});
+btnTwoLevel.addEventListener("click", () => {
+  menuLevelTwo.classList.toggle("is-sicret");
+  levelTwo.classList.remove("is-sicret");
 });
